cbc8df6c78efac5ca26d5eac8d70aaf2f9009240
Vamsi Krishna Sure <vamsisk.git@outlook.com>
2016-05-15 20:15:42 +0100

    first commit and test case for finding minumum value in the list of values.

--- src/main/java/com/cleancode/tdd/CalculateStats.java
+++ src/main/java/com/cleancode/tdd/CalculateStats.java
@@ -3,10 +3,16 @@
  */
 package com.cleancode.tdd;
 
+import java.util.List;
+
 /**
  * @author vsure
  *
  */
 public class CalculateStats {
+
+	public Integer getMinValue(List<Integer> dataList) {
+		return null;
+	}
 
 }
--- src/test/java/com/cleancode/tdd/CalculateStatsTest.java
+++ src/test/java/com/cleancode/tdd/CalculateStatsTest.java
@@ -1,6 +1,10 @@
 package com.cleancode.tdd;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
@@ -8,8 +12,11 @@ import org.junit.Test;
 
 public class CalculateStatsTest {
 
+	CalculateStats calculateStats;
+
 	@Before
 	public void setUp() throws Exception {
+		calculateStats = new CalculateStats();
 	}
 
 	@After
@@ -17,8 +24,14 @@ public class CalculateStatsTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void returnMinValueFromListTest() {
+
+		List<Integer> dataList = Arrays.asList(6, 9, 15, -2, 92, 11);
+
+		Integer minValue = calculateStats.getMinValue(dataList);
+
+		assertNotNull("Minumum value returned. ", minValue);
+		assertEquals("Wrong minumum value returned.", -2, minValue.intValue());
 	}
 
 }
