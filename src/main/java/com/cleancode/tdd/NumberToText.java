2b4e1fe8297758adbf8e1f25c4e245f58a0282e6
Vamsi Krishna Sure <vamsisk.git@outlook.com>
2016-05-22 18:28:55 +0100

    Double digit test cases included

--- src/main/java/com/cleancode/tdd/NumberToText.java
+++ src/main/java/com/cleancode/tdd/NumberToText.java
@@ -2,4 +2,18 @@ package com.cleancode.tdd;
 
 public class NumberToText {
 
+	private String[] names = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+			"Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+
+	public String getText(int number) {
+
+		StringBuilder text = new StringBuilder();
+
+		if (number < 20 && number > 0) {
+			text.append(names[number]);
+		}
+
+		return text.toString();
+	}
+
 }
--- src/test/java/com/cleancode/tdd/NumberToTextTest.java
+++ src/test/java/com/cleancode/tdd/NumberToTextTest.java
@@ -2,14 +2,30 @@ package com.cleancode.tdd;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
 public class NumberToTextTest {
 
 	private NumberToText numberToText = new NumberToText();
 
-	public void testSingleDitiNumber() {
+	@Test
+	public void testSingleDigitNumberSeven() {
 		String text = numberToText.getText(7);
 
 		assertEquals("Seven", text);
 	}
 
+	@Test
+	public void testDoubleDigitNumberSeventeen() {
+		String text = numberToText.getText(17);
+
+		assertEquals("Seventeen", text);
+	}
+
+	@Test
+	public void testDoubleDigitNumberSixtyEight() {
+		String text = numberToText.getText(68);
+
+		assertEquals("Sixty Eight", text);
+	}
 }
