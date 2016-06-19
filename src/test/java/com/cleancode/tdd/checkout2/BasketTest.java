2f1c469ce875dc6ca57725a9763444c7152cd6f5
Vamsi Krishna Sure <vamsisk.git@outlook.com>
2016-06-20 00:32:19 +0100

    Test case for Basket.addSku to process multiple Skus

--- src/test/java/com/cleancode/tdd/checkout2/BasketTest.java
+++ src/test/java/com/cleancode/tdd/checkout2/BasketTest.java
@@ -1,10 +1,14 @@
 package com.cleancode.tdd.checkout2;
 
 import static com.cleancode.tdd.checkout2.Sku.A;
+import static com.cleancode.tdd.checkout2.Sku.B;
+import static com.cleancode.tdd.checkout2.Sku.C;
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
-
 
 public class BasketTest {
 
@@ -17,5 +21,14 @@ public class BasketTest {
 		assertSame(3, basket.getSku(A));
 		basket.resetItemCountFor(A);
 		assertSame(0, basket.getSku(A));
+	}
+
+	@Test
+	public void testMultiAddOfSkus() {
+		Basket basket = new Basket();
+		basket.addSku(asList(A, B, C, A));
+		assertThat(2, is(basket.getSku(A)));
+		assertThat(1, is(basket.getSku(B)));
+		assertThat(1, is(basket.getSku(C)));
 	}
 }
