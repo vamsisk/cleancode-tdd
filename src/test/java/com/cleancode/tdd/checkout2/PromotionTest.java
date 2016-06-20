03c6f62c5424afeef4f8c81e559589405cc9f9ce
Vamsi Krishna Sure <vamsisk.git@outlook.com>
2016-06-20 23:18:58 +0100

    added test cases for testing promotion B

--- src/test/java/com/cleancode/tdd/checkout2/PromotionTest.java
+++ src/test/java/com/cleancode/tdd/checkout2/PromotionTest.java
@@ -4,6 +4,7 @@
 package com.cleancode.tdd.checkout2;
 
 import static com.cleancode.tdd.checkout2.Sku.A;
+import static com.cleancode.tdd.checkout2.Sku.B;
 import static org.hamcrest.CoreMatchers.is;
 
 import org.junit.Assert;
@@ -14,6 +15,46 @@ import org.junit.Test;
  *
  */
 public class PromotionTest {
+
+	@Test
+	public void returnPromotionPriceIf_sku_count_Matches_For_4_SkuB() {
+		Promotion promotion = new Promotion(Sku.B, 2, 45);
+
+		Checkout checkout = new Checkout(promotion);
+		checkout.scan(B, B, B, B);
+
+		Assert.assertThat(90, is(promotion.applyAndReturnUpdatedTotal(checkout.getBasket())));
+	}
+
+	@Test
+	public void returnPromotionPriceIf_sku_count_Matches_For_3_SkuB() {
+		Promotion promotion = new Promotion(Sku.B, 2, 45);
+
+		Checkout checkout = new Checkout(promotion);
+		checkout.scan(B, B, B);
+
+		Assert.assertThat(75, is(promotion.applyAndReturnUpdatedTotal(checkout.getBasket())));
+	}
+
+	@Test
+	public void returnPromotionPriceIf_sku_count_Matches_For_2_SkuB() {
+		Promotion promotion = new Promotion(Sku.B, 2, 45);
+
+		Checkout checkout = new Checkout(promotion);
+		checkout.scan(B, B);
+
+		Assert.assertThat(45, is(promotion.applyAndReturnUpdatedTotal(checkout.getBasket())));
+	}
+
+	@Test
+	public void returnPromotionPriceIf_sku_count_Matches_For_1_SkuB() {
+		Promotion promotion = new Promotion(Sku.B, 2, 45);
+
+		Checkout checkout = new Checkout(promotion);
+		checkout.scan(B);
+
+		Assert.assertThat(30, is(promotion.applyAndReturnUpdatedTotal(checkout.getBasket())));
+	}
 
 	@Test
 	public void returnPromotionPriceIfMathces3ForSkuA() {
